6cbc806b3b9f6ab31bcf223142a885e14caa3676
divijg19 <divijganjoo2003@gmail.com>
2026-01-24 00:40:59 +0530

    v0.0.1: apply basic movement actions during tick

--- internal/runtime/tick.go
+++ internal/runtime/tick.go
@@ -1,6 +1,9 @@
 package runtime
 
-import "github.com/divijg19/Nightshade/internal/agent"
+import (
+	"github.com/divijg19/Nightshade/internal/agent"
+	"github.com/divijg19/Nightshade/internal/world"
+)
 
 type Decisions map[string]agent.Action
 
@@ -10,6 +13,13 @@ func (r *Runtime) TickOnce() Decisions {
 		snapshot := r.snapshotFor(a)
 		action := a.Decide(snapshot)
 		decisions[a.ID()] = action
+
+		pos, ok := r.world.PositionOf(a.ID())
+		if !ok {
+			continue
+		}
+		newPos := applyMovement(pos, action)
+		r.world.SetPosition(a.ID(), newPos)
 	}
 	r.advanceTick()
 	return decisions
@@ -29,4 +39,19 @@ func (r *Runtime) snapshotFor(a agent.Agent) Snapshot {
 	}
 
 	return snap
+}
+
+func applyMovement(pos world.Position, action agent.Action) world.Position {
+	switch action {
+	case 0: // MOVE_N
+		return world.Position{X: pos.X, Y: pos.Y - 1}
+	case 1: // MOVE_S
+		return world.Position{X: pos.X, Y: pos.Y + 1}
+	case 2: // MOVE_E
+		return world.Position{X: pos.X + 1, Y: pos.Y}
+	case 3: // MOVE_W
+		return world.Position{X: pos.X - 1, Y: pos.Y}
+	default:
+		return pos
+	}
 }
