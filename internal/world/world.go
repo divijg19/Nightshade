3f488201308783455fb4dac009fd578d9c2a36a9
divijg19 <divijganjoo2003@gmail.com>
2026-01-24 00:27:48 +0530

    v0.0.1: introduce minimal world state container

--- internal/world/world.go
+++ internal/world/world.go
@@ -1,1 +1,24 @@
 package world
+
+type Position struct {
+	X, Y int
+}
+
+type World struct {
+	entities map[string]Position
+}
+
+func New() *World {
+	return &World{
+		entities: make(map[string]Position),
+	}
+}
+
+func (w *World) PositionOf(id string) (Position, bool) {
+	pos, ok := w.entities[id]
+	return pos, ok
+}
+
+func (w *World) SetPosition(id string, pos Position) {
+	w.entities[id] = pos
+}
